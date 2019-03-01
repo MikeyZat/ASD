61891b04bc97a69838bb27096ff6344719a37ddf
MikeyZat <mikeyzat@gmail.com>
2019-03-01 15:36:06 +0100

    xD

---
+++ cmake-build-debug/CMakeFiles/ASD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ASD.dir/insertionSort.cpp.obj"
+  "ASD.pdb"
+  "ASD.exe"
+  "ASD.exe.manifest"
+  "ASD.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ASD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
